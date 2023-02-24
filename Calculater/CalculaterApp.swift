9bd24a4754e3a46752b03953ec76ed6dd32b98e4
amanialosaimi <amanialossaimi@gmail.com>
2023-02-24 15:14:22 +0300

    Initial Commit

---
+++ Calculater/CalculaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculaterApp.swift
+//  Calculater
+//
+//  Created by Amani Al Osaimi on 04/08/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct CalculaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
